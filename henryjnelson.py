d20ecaf8dd7f7e8fa3bc9f28657f5dd20e81745e
hnoslen <hnoslen@users.noreply.github.com>
2016-02-19 17:59:26 -0600

    Added ideas to index, started a few articles

--- henryjnelson.py
+++ henryjnelson.py
@@ -1,7 +1,6 @@
 import urllib
 import datetime
 import time
-import basepage
 import webapp2
 from google.appengine.ext import ndb
 from google.appengine.api import mail
@@ -17,35 +16,38 @@ def get_file(name):
         f.close()
     finally:
         return out
+        
+basepage = get_file("basepage.html")
 #class Comment(ndb.Model):
 #    page = ndb.StringProperty()
 #    text = ndb.TextProperty()
 #    time = ndb.DateTimeProperty(auto_now_add=True)
 #    votes = ndb.IntegerProperty()
 def message(mess):
-    return basepage.base+mess+"</body></html>"
+    return basepage+mess+"</body></html>"
 class Contact(webapp2.RequestHandler):
     def get(self):
-        p = basepage.base+"""<form action="/contact" method="POST" id="contact_form">
+        p = basepage+"""<h2>Contact Form</h2>
+<form action="/contact" method="POST" id="contact_form">
 Please only contact me by this method if there is no other method available. Also, please don't spam it. That's just mean.<br><br>
-<h2>Contact Form</h2>
-From: (how will I know how to get back to you)<br>
+From: (Name)<br>
 <input type="text" name="from" size="50" autofocus="autofocus"><br><br>
-Message:<br>
-<textarea name="message" form="contact_form" rows="10" cols="100"></textarea><br>
+Your Contact address (email):<br>
+<input type="text" name="email" size="50"><br><br>
+What are you trying to contact me about (100 characters): <br>
+<input type="text" name="message" size="100"><br>
 <input type="submit" value="Submit">
 </form>
-If you get redirected to the homepage, it worked.
-
-</body>
 </html>"""
         self.response.write(p)
     def post(self):
-            subject = self.request.get("from")
-            body = self.request.get("message")
-            mail.send_mail(sender, toperson, subject, body)
-            self.response.write(message("Thanks. Hopefully I'll get back to you within a few days."))
-
+            subject = self.request.get("from")+" from HenryJNelson contact form"
+            body = "The following was submitted to the contact form at HenryJNelson.com/contact:\n\n"+self.request.get("message")
+            try:
+            	mail.send_mail(sender, toperson, subject, body)
+            	self.response.write(message("Thanks. Hopefully I'll get back to you within a few days."))
+            except:
+            	self.response.write(message("I'm sorry. There was a problem sending your message. Please try again."))
 class index(webapp2.RequestHandler):
     def get(self):
         self.response.write(get_file("index.html"))
